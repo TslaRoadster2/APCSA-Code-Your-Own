9c3740d8963e3dcbb71ecdea8a83385e932aa05b
TslaRoadster2 <wobarber@outlook.com>
2023-05-17 19:08:25 -0700

    Create Main.java
    Initial code--just contains print statement

---
+++ Main.java
@@ -0,0 +1,7 @@
+package APCSA.APCSA_Code_Your_Own;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Loading Adventure Game for MS_DOS_3.5");
+    }
+}
