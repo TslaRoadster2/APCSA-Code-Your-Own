1ec66c044c0932bbe498915da57aea140ec88473
TslaRoadster2 <wobarber@outlook.com>
2023-05-19 11:46:11 -0700

    Format Main.java
    Autoformat

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 package APCSA.APCSA_Code_Your_Own;
 
 public class Main {
-    public static void main(String[] args){
-        for (int i = 0; i < 5; i++){
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
             System.out.println();
         }
 
@@ -12,23 +12,25 @@ public class Main {
 
         String input = "";
         System.out.println("Welcome to DOS! Type \"help\" for commads.");
-        
-       while (!gameStarted){
-        input = Utils.inputString("C:\\>", new String[] {"help", "dir", "start adventuregame.exe"}, "Invalid command. Type \"help\" for help.");
 
-        switch (input){
-            case "help":
-                System.out.println("commands:\n\tdir\t\t\tview the current directory\n\thelp\t\t\tlist available commands\n\tcd {dir}\t\tnavigate to directory \'dir\'\n\tstart {app.exe}\t\tstart executable \'app.exe\'");
-                break;
-            case "dir":
-                System.out.println("\t05/18/1986\t01:41 PM \t<DIR>\tadventuregame.exe");
-                break;
-            case "start adventuregame.exe":
-                gameStarted = true;
-                break;
-            default:
-                System.out.println("An unknown error has occured. Please try again.");
-        }
+        while (!gameStarted) {
+            input = Utils.inputString("C:\\>", new String[] { "help", "dir", "start adventuregame.exe" },
+                    "Invalid command. Type \"help\" for help.");
+
+            switch (input) {
+                case "help":
+                    System.out.println(
+                            "commands:\n\tdir\t\t\tview the current directory\n\thelp\t\t\tlist available commands\n\tcd {dir}\t\tnavigate to directory \'dir\'\n\tstart {app.exe}\t\tstart executable \'app.exe\'");
+                    break;
+                case "dir":
+                    System.out.println("\t05/18/1986\t01:41 PM \t<DIR>\tadventuregame.exe");
+                    break;
+                case "start adventuregame.exe":
+                    gameStarted = true;
+                    break;
+                default:
+                    System.out.println("An unknown error has occured. Please try again.");
+            }
         }
     }
 }
