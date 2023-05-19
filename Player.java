06ba4ad66d3a3d57dacc41ec2b5fe6796d236b6f
TslaRoadster2 <wobarber@outlook.com>
2023-05-19 11:45:41 -0700

    Create Player.java
    Contains information about the player (direction and inventory)

---
+++ Player.java
@@ -0,0 +1,44 @@
+package APCSA.APCSA_Code_Your_Own;
+
+import java.util.ArrayList;;
+
+public class Player {
+    private String direction;
+    private ArrayList<Item> inventory;
+
+    public Player(String direction){
+        this.direction = direction;
+        this.inventory = new ArrayList<Item>();
+    }
+
+    public Player (String direction, ArrayList<Item> initialInventory){
+        this.direction = direction;
+        this.inventory = initialInventory;
+    }
+
+    public void setDirection(String direction){
+        this.direction = direction;
+    }
+
+    public String getDirection(){
+        return direction;
+    }
+
+    public void addItem(Item item){
+        inventory.add(item);
+    }
+
+    public Item getItem(int index){
+        return inventory.get(index);
+    }
+
+    public ArrayList<Item> getInventory(){
+        return inventory;
+    }
+
+    public void removeItem(int index){
+        inventory.remove(index);
+    }
+
+
+}
