8b93050abe17a88173f643e6b78b97ac79178554
TslaRoadster2 <wobarber@outlook.com>
2023-06-06 22:08:02 -0700

    Update Item.java
    Added item name, constructors, toString, and a getter method for hidden

--- Item.java
+++ Item.java
@@ -4,4 +4,24 @@ public class Item {
     private boolean hidden;
     private String description;
     private String ability;
+    private String name;
+
+    public Item(boolean hidden, String description, String name){
+        this(hidden, description, "none", name);
+    }
+
+    public Item(boolean hidden, String description, String ability, String name){
+        this.hidden = hidden;
+        this.description = description;
+        this.ability = ability;
+        this.name = name;
+    }
+
+    public boolean isHidden(){
+        return hidden;
+    }
+
+    public String toString(){
+        return name + ". It is " + description + "Its ability is as follows: " + ability + ".";
+    }
 }
