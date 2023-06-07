ac8ba941caf6b8f0ef9208e1380b44b8689fdc30
TslaRoadster2 <wobarber@outlook.com>
2023-06-06 22:09:05 -0700

    Update Player.java
    Added current player position and list of visited rooms, and applicable getter/setter methods. Added toString.

--- Player.java
+++ Player.java
@@ -1,23 +1,38 @@
 package APCSA.APCSA_Code_Your_Own;
 
-import java.util.ArrayList;;
+import java.util.ArrayList;
 
 public class Player {
     private String direction;
     private ArrayList<Item> inventory;
-
-    public Player(String direction) {
-        this.direction = direction;
-        this.inventory = new ArrayList<Item>();
-    }
+    private int currentRow = 0;
+    private int currentColumn = 0;
+    private ArrayList<Room> visited;
 
     public Player(String direction, ArrayList<Item> initialInventory) {
         this.direction = direction;
         this.inventory = initialInventory;
+        this.visited = new ArrayList<Room>();
     }
 
     public void setDirection(String direction) {
         this.direction = direction;
+    }
+
+    public void setRow(int row){
+        this.currentRow = row;
+    }
+
+    public void setColumn(int column){
+        this.currentColumn = column;
+    }
+
+    public void addVisited(Room room){
+        visited.add(room);
+    }
+
+    public ArrayList<Room> getVisited(){
+        return visited;
     }
 
     public String getDirection() {
@@ -38,6 +53,30 @@ public class Player {
 
     public void removeItem(int index) {
         inventory.remove(index);
+    }
+
+    public int getRow(){
+        return currentRow;
+    }
+
+    public int getColumn(){
+        return currentColumn;
+    }
+
+    public String getLocation(){
+        return "You are facing direction " + direction + ", and are in the room at coordinates (" + currentRow + ", "  + currentColumn + "). ";
+    }
+
+    public String getInventoryString(){
+        String returnString = "Your inventory contains:\n";
+        for (Item item: inventory){
+            returnString += item + "\n";
+        }
+        return returnString;
+    }
+
+    public String toString(){
+        return getLocation() + getInventoryString();
     }
 
 }
