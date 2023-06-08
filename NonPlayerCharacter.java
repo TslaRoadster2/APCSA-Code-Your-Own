bad8f075cf0b5e067b53631ea43fb468acc277e8
TslaRoadster2 <wobarber@outlook.com>
2023-06-07 22:28:45 -0700

    Create NonPlayerCharacter.java
    Created NPC to be used for enemies that the player must combat.

---
+++ NonPlayerCharacter.java
@@ -0,0 +1,47 @@
+package APCSA.APCSA_Code_Your_Own;
+
+public class NonPlayerCharacter {
+    private int maxHealth;
+    private int currentHealth;
+    private int attackDamage;
+    private String name;
+
+    public NonPlayerCharacter(int maxHealth, int attackDamage, String name) {
+        this.maxHealth = maxHealth;
+        this.currentHealth = maxHealth;
+        this.attackDamage = attackDamage;
+        this.name = name;
+    }
+
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public int getAttackDamage() {
+        return attackDamage;
+    }
+
+    public boolean takeDamage(int damage) {
+        currentHealth -= damage;
+        return currentHealth > 0;
+    }
+
+    public void attackPlayer(Player targetPlayer, int damage) {
+        targetPlayer.takeDamage(damage);
+    }
+
+    public void renewHealth() {
+        currentHealth = maxHealth;
+    }
+
+    public void renewHealth(int health) {
+        currentHealth += health;
+        if (currentHealth > maxHealth) {
+            currentHealth = maxHealth;
+        }
+    }
+
+    public String toString() {
+        return name + " with health " + currentHealth + "//" + maxHealth + ".";
+    }
+}
