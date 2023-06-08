d898c0eb64cf7fd6f42439fcbf761544308c8ea6
TslaRoadster2 <wobarber@outlook.com>
2023-06-07 22:27:49 -0700

    Update WeaponItem.java
    Added functionality

--- WeaponItem.java
+++ WeaponItem.java
@@ -1,5 +1,37 @@
 package APCSA.APCSA_Code_Your_Own;
 
-public class WeaponItem extends Item{
-    
+public class WeaponItem extends Item {
+    private int damageInfliction;
+    private int maxDurability;
+    private int currentDurability;
+    private NonPlayerCharacter target;
+
+    public WeaponItem(String description, int damageInfliction, int MaxDurability) {
+        super(description, new String[] { "attack" });
+        this.damageInfliction = damageInfliction;
+        this.maxDurability = MaxDurability;
+        this.currentDurability = currentDurability;
+    }
+
+    public boolean useAbility(String selectedAbility, Player player) {
+        if (hasAbility(selectedAbility)) {
+            attack();
+            return true;
+        }
+        return false;
+    }
+
+    public void setTarget(NonPlayerCharacter target) {
+        this.target = target;
+    }
+
+    private boolean attack() {
+        target.takeDamage(damageInfliction);
+        currentDurability -= 2;
+        return currentDurability < 0;
+    }
+
+    public void repair() {
+        currentDurability = maxDurability;
+    }
 }
