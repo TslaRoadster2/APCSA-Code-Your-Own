72683d1471c94e3efbb38fbe030f5d85fd11aff9
TslaRoadster2 <wobarber@outlook.com>
2023-06-07 22:29:45 -0700

    Update MapItem.java
    Removed "hidden" boolean to match parent class

--- MapItem.java
+++ MapItem.java
@@ -1,22 +1,22 @@
 package APCSA.APCSA_Code_Your_Own;
 
-public class MapItem extends Item{
+public class MapItem extends Item {
     private Map map;
 
-    public MapItem(Map map){
-        super(false, "a map of the rooms. You'll fill it in as you go. ", new String[] {"view map"});
+    public MapItem(Map map) {
+        super("a map of the rooms. You'll fill it in as you go. ", new String[] { "view map" });
         this.map = map;
     }
 
-    public boolean useAbility(String selectedAbility, Player player){
-        if (hasAbility(selectedAbility)){
+    public boolean useAbility(String selectedAbility, Player player) {
+        if (hasAbility(selectedAbility)) {
             printMap(player);
             return true;
         }
-        return false;        
+        return false;
     }
-    
-    public void printMap(Player player){
+
+    private void printMap(Player player) {
         System.out.println("\n\n" + map.toPrettyString(player) + "\n");
     }
 }
